00036102e2ba9477cb6fcf3f8c8c0fdfe99471df
Risto Hietala <risto@hietala.org>
2022-02-09 11:42:37 +0200

    analysointiin säätöä

--- src/ride_the_wave.rs
+++ src/ride_the_wave.rs
@@ -1,15 +1,25 @@
 extern crate diesel;
 
+use crate::schema::shortlist;
 use diesel::prelude::*;
 use diesel::{delete, sql_query};
-use crate::schema::shortlist;
 
-pub fn analyze(connection: &PgConnection, base: String, period: i32) -> Result<usize, diesel::result::Error> {
+const MA_SHORT: i32 = 10;
+const MA_MED: i32 = 30;
+const MA_LONG: i32 = 200;
+
+pub fn analyze(
+    connection: &PgConnection,
+    base: String,
+    period: i32,
+) -> Result<usize, diesel::result::Error> {
     println!("analyzing");
 
+    let max_seconds = period * MA_LONG;
+
     delete(shortlist::table).execute(connection)?;
-    sql_query(
-        format!("
+    sql_query(format!(
+        "
       WITH filtered_symbols AS (
         SELECT
           quote
@@ -18,7 +28,7 @@ pub fn analyze(connection: &PgConnection, base: String, period: i32) -> Result<u
         WHERE
           base = '{base}'
           AND period = {period}
-          AND timestamp > (current_timestamp - interval '24 hours')
+          AND timestamp > (current_timestamp - interval '{max_seconds} seconds')
           -- filter out instruments
           AND quote NOT LIKE '%BULL'
           AND quote NOT LIKE '%BEAR'
@@ -34,39 +44,41 @@ pub fn analyze(connection: &PgConnection, base: String, period: i32) -> Result<u
           -- filter out those with too small minimum quote value
           -- (these have too high %-change with single pips)
           AND MIN(average) > 1e-6
+          -- filter out those that are missing more than 5 candles from the longest MA period
+          AND count(*) > ({max_seconds} / {period}) - 5
       ),
       analyzed AS (
         SELECT
           DISTINCT ON (quote) quote,
           timestamp,
           average,
-          -- average over previous 10 rows
+          -- short moving average
           AVG(average) OVER(
             PARTITION BY quote,
             base,
             period
             ORDER BY
-              timestamp ROWS BETWEEN 10 PRECEDING
+              timestamp ROWS BETWEEN {ma_short} PRECEDING
               AND CURRENT ROW
-          ) AS ma10,
-          -- average over previous 30 rows
+          ) AS ma_short,
+          -- medium moving average
           AVG(average) OVER(
             PARTITION BY quote,
             base,
             period
             ORDER BY
-              timestamp ROWS BETWEEN 30 PRECEDING
+              timestamp ROWS BETWEEN {ma_med} PRECEDING
               AND CURRENT ROW
-          ) AS ma30,
-          -- average over previous 200 rows
+          ) AS ma_med,
+          -- long moving average
           AVG(average) OVER(
             PARTITION BY quote,
             base,
             period
             ORDER BY
-              timestamp ROWS BETWEEN 200 PRECEDING
+              timestamp ROWS BETWEEN {ma_long} PRECEDING
               AND CURRENT ROW
-          ) AS ma200
+          ) AS ma_long
         FROM
           candles
         WHERE
@@ -81,22 +93,28 @@ pub fn analyze(connection: &PgConnection, base: String, period: i32) -> Result<u
         quote,
         NOW(),
         average,
-        average / ma30 as confidence
+        average / ma_med as confidence
       FROM
         (SELECT * FROM analyzed) AS analyzed
       WHERE
         (
           -- actual logic: current value must be above 10-period moving average,
           -- which must be above 30-period MA, which must be above 200-period MA
-          average > ma10
-          AND ma10 > ma30
-          AND ma30 > ma200
+          average > ma_short
+          AND ma_short > ma_med
+          AND ma_med > ma_long
           -- filter out too small changes from moving average (stablecoins)
-          AND average / ma30 > 0.001
+          AND (average / ma_med) > 1.001
           -- and too large changes, too strange situations
-          AND average / ma30 < 0.100
+          AND (average / ma_med) < 1.100
         ) is true);
-    ", base = base, period = period),
-    )
+    ",
+        base = base,
+        period = period,
+        max_seconds = max_seconds,
+        ma_short = MA_SHORT,
+        ma_med = MA_MED,
+        ma_long = MA_LONG
+    ))
     .execute(connection)
 }
